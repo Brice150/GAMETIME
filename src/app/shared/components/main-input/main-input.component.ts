e847ffec58e9b4dfe3f2f7ab52173acb3aa26de5
Brice150 <brice.lecomte0@gmail.com>
2023-06-12 22:24:30 +0200

    new word and country lists in french

--- src/app/shared/components/main-input/main-input.component.ts
+++ src/app/shared/components/main-input/main-input.component.ts
@@ -9,6 +9,8 @@ export class MainInputComponent implements OnInit {
   @Input() response!: string;
   wordToFind!: string;
   maxlength!: number;
+  inputValue!: string | null;
+  tries: string[] = [];
 
   ngOnInit() {
     if (this.response) {
@@ -23,6 +25,25 @@ export class MainInputComponent implements OnInit {
   
     if (!isAllowedCharacter) {
       event.preventDefault();
+    }
+  }
+
+  submitForm() {
+    if (this.inputValue && this.inputValue.length === this.maxlength) {
+      if (this.inputValue === this.response) {
+        console.log("You win");
+      }
+      else {
+        let letters = [];
+        for (let i = 0; i < this.response.length; i++) {
+          if (this.inputValue.includes(this.response[i])) {
+            letters.push(this.response[i]);
+          }
+        }
+        console.log(letters);
+      }
+      this.tries.push(this.inputValue);
+      this.inputValue = null;
     }
   }
 }
