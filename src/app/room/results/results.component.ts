b3ddbd8325917d8a5923892011ede7ffd7f454d3
brice150 <brice.lecomte0@gmail.com>
2025-08-08 16:40:24 +0200

    podium change

--- src/app/room/results/results.component.ts
+++ src/app/room/results/results.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, input } from '@angular/core';
+import { Component, EventEmitter, input, Output } from '@angular/core';
 import { Player } from 'src/app/core/interfaces/player';
 import { Room } from 'src/app/core/interfaces/room';
 import { DurationBetweenDatesPipe } from 'src/app/shared/pipes/duration.pipe';
@@ -15,4 +15,9 @@ export class ResultsComponent {
   room = input.required<Room>();
   player = input.required<Player>();
   players = input.required<Player[]>();
+  @Output() deleteEvent = new EventEmitter<string>();
+
+  delete(userId: string): void {
+    this.deleteEvent.emit(userId);
+  }
 }
