b237f8ab0b3aa60db852eafd8540fa25517d52d3
brice150 <brice.lecomte0@gmail.com>
2025-08-27 22:03:38 +0200

    fix

--- src/app/room/waiting-room/waiting-room.component.ts
+++ src/app/room/waiting-room/waiting-room.component.ts
@@ -3,11 +3,10 @@ import { Component, EventEmitter, input, Output } from '@angular/core';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { Player } from 'src/app/core/interfaces/player';
 import { Room } from 'src/app/core/interfaces/room';
-import { MedalsNumberPipe } from '../../shared/pipes/medals-number.pipe';
 
 @Component({
   selector: 'app-waiting-room',
-  imports: [CommonModule, MatProgressSpinnerModule, MedalsNumberPipe],
+  imports: [CommonModule, MatProgressSpinnerModule],
   templateUrl: './waiting-room.component.html',
   styleUrl: './waiting-room.component.css',
 })
