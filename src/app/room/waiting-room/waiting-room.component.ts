ffd5979fcb9f0dd8df05699dea19f71b2c3a2a40
brice150 <brice.lecomte0@gmail.com>
2025-09-20 21:37:37 +0200

    waiting room improved

--- src/app/room/waiting-room/waiting-room.component.ts
+++ src/app/room/waiting-room/waiting-room.component.ts
@@ -3,10 +3,11 @@ import { Component, EventEmitter, input, Output } from '@angular/core';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { Player } from 'src/app/core/interfaces/player';
 import { Room } from 'src/app/core/interfaces/room';
+import { TotalMedalsNumberPipe } from '../../shared/pipes/total-medals-number.pipe';
 
 @Component({
   selector: 'app-waiting-room',
-  imports: [CommonModule, MatProgressSpinnerModule],
+  imports: [CommonModule, MatProgressSpinnerModule, TotalMedalsNumberPipe],
   templateUrl: './waiting-room.component.html',
   styleUrl: './waiting-room.component.css',
 })
