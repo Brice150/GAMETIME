bc478d3f13332f27ac0a8deeb1e21e33f60aba15
brice150 <brice.lecomte0@gmail.com>
2025-09-21 16:51:21 +0200

    fix

--- src/app/admin/room-card/room-card.component.ts
+++ src/app/admin/room-card/room-card.component.ts
@@ -3,11 +3,11 @@ import { Component, EventEmitter, input, Output } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { Player } from 'src/app/core/interfaces/player';
 import { Room } from 'src/app/core/interfaces/room';
-import { MedalsNumberPipe } from '../../shared/pipes/medals-number.pipe';
+import { TotalMedalsNumberPipe } from '../../shared/pipes/total-medals-number.pipe';
 
 @Component({
   selector: 'app-room-card',
-  imports: [CommonModule, RouterModule, MedalsNumberPipe],
+  imports: [CommonModule, RouterModule, TotalMedalsNumberPipe],
   templateUrl: './room-card.component.html',
   styleUrl: './room-card.component.css',
 })
