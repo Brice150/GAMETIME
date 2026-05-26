d1f064e2eecf8d873a3d052e89483f23ec1c9f61
Brice150 <brice.lecomte0@gmail.com>
2026-05-26 14:39:57 +0200

    preloading

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -16,10 +16,15 @@ import { getAuth, GoogleAuthProvider, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { MAT_DATE_LOCALE } from '@angular/material/core';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-import { provideRouter } from '@angular/router';
+import {
+  provideRouter,
+  withInMemoryScrolling,
+  withPreloading,
+} from '@angular/router';
 import { provideToastr } from 'ngx-toastr';
 import { environment } from '../environments/environment';
 import { routes } from './app.routes';
+import { SmartPreloading } from './core/services/smart-preloading.service';
 
 const firebaseApp: FirebaseApp = initializeApp(environment.firebase);
 
@@ -30,7 +35,14 @@ registerLocaleData(localeFr);
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
+    provideRouter(
+      routes,
+      withInMemoryScrolling({
+        scrollPositionRestoration: 'top',
+        anchorScrolling: 'enabled',
+      }),
+      withPreloading(SmartPreloading),
+    ),
     provideToastr(),
     provideAnimationsAsync(),
     provideHttpClient(),
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,31 +1,57 @@
 import { Routes } from '@angular/router';
-import { AdminRoomComponent } from './admin-room/admin-room.component';
-import { AdminComponent } from './admin/admin.component';
-import { ConnectComponent } from './connect/connect.component';
 import { adminGuard } from './core/guards/admin.guard';
 import { noUserGuard } from './core/guards/no-user.guard';
 import { userGuard } from './core/guards/user.guard';
-import { HomeComponent } from './home/home.component';
-import { ProfileComponent } from './profile/profile.component';
-import { RoomComponent } from './room/room.component';
-import { RankingComponent } from './ranking/ranking.component';
-import { SuccessComponent } from './success/success.component';
 
 export const routes: Routes = [
-  { path: 'connect', component: ConnectComponent, canActivate: [noUserGuard] },
-  { path: '', component: HomeComponent, canActivate: [userGuard] },
-  { path: 'profil', component: ProfileComponent, canActivate: [userGuard] },
-  { path: 'succes', component: SuccessComponent, canActivate: [userGuard] },
-  { path: 'classement', component: RankingComponent, canActivate: [userGuard] },
   {
-    path: 'room/:id',
-    component: RoomComponent,
+    path: 'connect',
+    loadComponent: () =>
+      import('./connect/connect.component').then((m) => m.ConnectComponent),
+    canActivate: [noUserGuard],
+  },
+  {
+    path: '',
+    loadComponent: () =>
+      import('./home/home.component').then((m) => m.HomeComponent),
     canActivate: [userGuard],
   },
-  { path: 'admin', component: AdminComponent, canActivate: [adminGuard] },
+  {
+    path: 'profil',
+    loadComponent: () =>
+      import('./profile/profile.component').then((m) => m.ProfileComponent),
+    canActivate: [userGuard],
+  },
+  {
+    path: 'succes',
+    loadComponent: () =>
+      import('./success/success.component').then((m) => m.SuccessComponent),
+    canActivate: [userGuard],
+  },
+  {
+    path: 'classement',
+    loadComponent: () =>
+      import('./ranking/ranking.component').then((m) => m.RankingComponent),
+    canActivate: [userGuard],
+  },
+  {
+    path: 'room/:id',
+    loadComponent: () =>
+      import('./room/room.component').then((m) => m.RoomComponent),
+    canActivate: [userGuard],
+  },
+  {
+    path: 'admin',
+    loadComponent: () =>
+      import('./admin/admin.component').then((m) => m.AdminComponent),
+    canActivate: [adminGuard],
+  },
   {
     path: 'admin/:id',
-    component: AdminRoomComponent,
+    loadComponent: () =>
+      import('./admin-room/admin-room.component').then(
+        (m) => m.AdminRoomComponent,
+      ),
     canActivate: [adminGuard],
   },
   { path: '**', redirectTo: '', pathMatch: 'full' },
